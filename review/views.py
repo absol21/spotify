3988d55829053fcd9204c13732a045c67fb0137e
kanyshai0711 <abdirashitovakanyshai99@gmail.com>
2023-06-05 20:22:10 +0600

    .

---
+++ review/views.py
@@ -0,0 +1,30 @@
+from .serializers import CommentSerializer, RatingSerializer
+from .models import Comment, Rating,Playlist
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from .permissions import IsAuthorOrReadOnly
+
+
+class PermissionMixin:
+    def get_permissions(self):
+        if self.action == 'create':
+            permissions = [IsAuthenticated]
+        elif self.action in ['update', 'partial_update','destroy']:
+            permissions = [IsAuthorOrReadOnly]
+        else:
+            permissions = [AllowAny]
+        return [permission() for permission in permissions]
+
+
+class CommentViewSet(PermissionMixin,ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    
+class RatingViewSet(PermissionMixin,ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+
+class PlaylistViewSet(PermissionMixin,ModelViewSet):
+    queryset = Playlist.objects.all()
+    serializer_class = RatingSerializer
