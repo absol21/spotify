0bcaf358370fecf0112faf19c72b164b5bd4ced8
Emka06 <kadyrbekovemir435@gmail.com>
2023-06-13 15:24:37 +0600

    .

--- account/models.py
+++ account/models.py
@@ -1,65 +1,55 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
 from django.utils.crypto import get_random_string
 
 
 class UserManager(BaseUserManager):
-    def _create(self, email, password,
-                **extra_fields):
+    def create_user(self, email, password, **extra_fields):
         if not email:
-            raise ValueError('Поле email не '
-                'может быть пустым')
+            raise ValueError('Email field is required')
         email = self.normalize_email(email)
-        # self.model = User
-        
-        user = self.model(email=email,
-                          **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.create_activation_code()
         user.save()
         return user
 
-    def create_user(self, email, password,
-                    **extra_fields):
-        # extra_fields.setdefault('is_active', True)
-        return self._create(email, password,
-                            **extra_fields)
+    def create_superuser(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Email field is required')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
 
-    def create_superuser(self, email, password,
-                         **extra_fields):
-        
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_active', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self._create(email, password,
-                            **extra_fields)
-
-
-class User(AbstractUser):
-    email = models.EmailField(
-        unique=True)
+class User(AbstractBaseUser):
     username = None
-    is_active = models.BooleanField(
-        default=False
-    )
-    activation_code = models.CharField(
-        max_length=20, blank=True
-    )
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_super_user = models.BooleanField(default=False)
+    activation_code = models.CharField(max_length=10, blank=True)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    def __str__(self):
-        return f'{self.id} -> {self.email}'
-
-    # def has_module_perms(self, app_label):
-    #     return self.is_staff
-
-    # def has_perm(self, perm, obj=None):
-    #     return self.is_staff
+    def __str__(self) -> str:
+        return f'{self.email} -> {self.id}'
+    
+    def has_module_perms(self, app_label):
+        return self.is_staff
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_staff
 
     def create_activation_code(self):
-        code = get_random_string(10, allowed_chars='123456789#@!$%^&*_')
-        self.activation_code=code
+        code = get_random_string(length=10, allowed_chars='0123456789')
+        self.activation_code = code
+        self.save()
--- account/permissions.py
+++ account/permissions.py
@@ -3,3 +3,7 @@ from rest_framework.permissions import BasePermission
 class IsActivePermissions(BasePermission):
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_active)
+    
+class IsPremiumUserPermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_premium_user
