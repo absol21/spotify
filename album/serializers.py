f2c950f0df51772207a324c60d7346791d1ef686
absol21 <2006ermek2006@gmail.com>
2023-06-14 18:52:49 +0600

    .

--- album/serializers.py
+++ album/serializers.py
@@ -35,21 +35,21 @@ def create(self, validated_data):
 
     
 
-    class AlbumSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Album
-            fields = ('title', 'image', 'created_at', 'audio_files')
+class AlbumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Album
+        fields = ('title', 'image', 'created_at', 'audio_files')
 
-        def create(self, validated_data):
-            request = self.context.get('request')
-            user = request.user
-            audio_files_data = validated_data.pop('audio_files')
-            album = Album.objects.create(author=user, **validated_data)
-            album.audio_files.set(audio_files_data)
-            return album
+    def create(self, validated_data):
+        request = self.context.get('request')
+        user = request.user
+        audio_files_data = validated_data.pop('audio_files')
+        album = Album.objects.create(author=user, **validated_data)
+        album.audio_files.set(audio_files_data)
+        return album
 
-        def to_representation(self, instance):
-            representation = super().to_representation(instance)
-            representation['likes_count'] = instance.likes.count()
-            representation['rating_avg'] = instance.ratings.aggregate(Avg('rating'))['rating__avg']
-            return representation
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['likes_count'] = instance.likes.count()
+        representation['rating_avg'] = instance.ratings.aggregate(Avg('rating'))['rating__avg']
+        return representation
